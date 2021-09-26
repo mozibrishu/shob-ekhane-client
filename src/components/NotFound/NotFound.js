e44a58c46513e1ca4436f2bfd3e7860639a5284b
mozibrishu <mozib.diu@gmail.com>
2021-09-26 13:56:01 +0600

    not found and basic components added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{color: "red"}}>
+            NotFound
+        </div>
+    );
+};
+
+export default NotFound;
