cda4b1aefcae5c6a8e86ff266ac4adc89f1f4dea
mozibrishu <mozib.diu@gmail.com>
2021-10-05 08:52:41 +0600

    Header Complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../../App';
+import logo from '../../images/logo.png';
+import './Header.css';
 
 const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     return (
-        <div>
-            this is header.
+        <div className="header">
+            <img src={logo} alt="logo" />
+            <nav>
+                <Link to="/shop">Shop</Link>
+                <Link to="/review">Order Review</Link>
+                <Link to="/inventory">Manage Inventory</Link>
+                <button onClick={() => setLoggedInUser({})}>Sign Out</button>
+            </nav>
         </div>
     );
 };
